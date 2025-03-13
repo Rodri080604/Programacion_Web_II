ada4aba87e2dbd51f635bd14e66097047b317ef4
Rodri080604 <huarachirodrigo76@gmail.com>
2025-03-13 09:32:03 -0400

    Uso del dom

--- Practica5/Ejercicios.js
+++ Practica5/Ejercicios.js
@@ -1,4 +1,4 @@
-/*1____________________________________*/
+/*1.-Dado un array de números, devuelve un objeto con la cantidad de números pares e impares..*/
 let numerosArray = [7,5,52,3];
 let resultado = { pares: 0, impares: 0 };
 for (let i = 0; i < numerosArray.length; i++) {
